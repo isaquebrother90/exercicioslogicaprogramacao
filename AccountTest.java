228e49c5148dfcd38f7b1d485f1151c31f93ccf5
Isaque Moura <improgramcode@gmail.com>
2020-06-03 13:39:16 -0300

    Exercícios Account.java e AccountTest.java criados e resolvidos, capítulo 3 Deitel

---
+++ AccountTest.java
@@ -0,0 +1,58 @@
+// Entrada e saída de números de ponto flutuante com objetos Account.
+
+import java.util.Scanner;
+
+public class AccountTest {
+    public static void main(String[] args) {
+        Account account1 = new Account("Jane Green", 50.00);
+        Account account2 = new Account("John Blue", -7.53);
+
+        // exibe saldo inicial de cada objeto
+        System.out.printf("%s balance: $%.2f %n",
+                account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $%.2f %n%n",
+                account2.getName(), account2.getBalance());
+
+        // cria um Scanner para obter entrada a partir da janela de comando
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter deposit amount for account1: "); // prompt
+        double depositAmount = input.nextDouble(); // obtém a entrada do usuário1
+        System.out.printf("%nadding %.2f to account1 balance%n%n", depositAmount);
+        account1.deposit(depositAmount); // adiciona o saldo de account1
+
+        // exibe os saldos
+        System.out.printf("%s balance: $%.2f %n",
+                account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $%.2f %n%n",
+                account2.getName(), account2.getBalance());
+
+        System.out.print("Enter deposit amount for account2: "); // prompt
+        depositAmount = input.nextDouble(); // obtém a entrada do usuário2
+        System.out.printf("%nadding %.2f to account2 balance%n%n", depositAmount);
+        account2.deposit(depositAmount); // adiciona ao saldo de account2
+
+        // exibe os saldos
+        System.out.printf("%s balance: $%.2f %n",
+                account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $%.2f %n%n",
+                account2.getName(), account2.getBalance());
+
+        System.out.println("Enter withdraw amount for account1: ");// prompt
+        double withdrawAmount = input.nextDouble(); // obtém a entrada do usuário1
+        System.out.printf("%nwithdrawing %.2f from account1 balance%n%n", withdrawAmount);
+        account1.withdraw(withdrawAmount); // retira do saldo de account1
+
+        System.out.println("Enter withdraw amount for account2: ");// prompt
+        withdrawAmount = input.nextDouble(); // obtém a entrada do usuário2
+        System.out.printf("%nwithdrawing %.2f from account2 balance%n%n", withdrawAmount);
+        account2.withdraw(withdrawAmount); // retira do saldo de account2
+
+        // exibe os saldos
+        System.out.printf("%s balance: $%.2f %n",
+                account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $%.2f %n%n",
+                account2.getName(), account2.getBalance());
+
+    } // fim de main
+}// fim da classe AccountTest
